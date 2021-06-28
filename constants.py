a2fbe03a89053ba5729a2dc7cb42a7306e08d474
Булат <bulat@MacBook-Air-Bulat.local>
2021-06-28 22:28:00 +0300

    const.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-API_TOKEN = '1785319775:AAH26vdWig6APYgM3iXrFrmMqYjsELwdk9w'
+API_TOKEN = '1785319775' + ':AAH26vdWig6APYgM3iXrFrmMqYjsELwdk9w'
 MY_ID = "171961446"
 
 URL = 'https://955334aa72f7.ngrok.io'
