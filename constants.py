2e2d9fee6b1c58b9ee9f34f3d444041c6070027a
Булат <bulat@MacBook-Air-Bulat.local>
2021-06-28 22:22:23 +0300

    const.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+API_TOKEN = '1785319775:AAH26vdWig6APYgM3iXrFrmMqYjsELwdk9w'
+MY_ID = "171961446"
+
+URL = 'https://955334aa72f7.ngrok.io'
+
+
+DB_USER = 'root'
+DB_PASS = '12345678'
+DB_HOST = 'localhost'
+DB_NAME = 'DB'
